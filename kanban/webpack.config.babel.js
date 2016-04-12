f911f29e38e5e6c1ab8acb1cabf138221e2d85f4
spitis <silviu.pitis@gmail.com>
2016-04-12 17:37:47 -0400

    starter project ready

---
+++ kanban/webpack.config.babel.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
+  entry: {
+    app: PATHS.app,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+        include: PATHS.app,
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel?cacheDirectory',
+        include: PATHS.app,
+      },
+    ],
+  },
+};
+
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+
+    devtool: 'eval-source-map',
+
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST || '0.0.0.0',
+      port: process.env.PORT,
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true, // --save
+      }),
+    ],
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
